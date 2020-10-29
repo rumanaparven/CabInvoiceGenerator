3de2d22d383744f68b5b9244214b6230c93678c1
Rumana <rumanaparven16@gmail.com>
2020-10-29 16:24:52 +0530

    [Rumana] Add. Given Distance And Time Should Return Total Fare

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -7,7 +7,7 @@ namespace CabInvoiceGenerator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Cab Invoice Generator");
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMUIM);
             double fare = invoiceGenerator.CalculateFare(2.0, 5);
             Console.WriteLine("Fare : " + fare);
         }
